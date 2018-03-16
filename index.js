fe64a1cc23996552279c2e4a971046c0e0fc56f8
Robert D Gauthier <rgauthie@gmail.com>
2018-03-16 15:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function takeANumber (katzDeliLine, newCust){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
-    return "The."
+    return "There is nobody waiting to be served."
   } else {
     let front = katzDeliLine.shift();
-    return `
+    return `Currently serving ${front}`;
   }
 }
