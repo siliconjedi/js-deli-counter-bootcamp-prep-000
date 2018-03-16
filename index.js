0ae510db479fabc7340101afb97584f12374c1ef
Robert D Gauthier <rgauthie@gmail.com>
2018-03-16 15:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber (katzDeliLine, newCust){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
-    return "The line is currently empty."
+    return "The."
   } else {
     let front = katzDeliLine.shift();
     return `
