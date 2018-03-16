4ab0f843ccefeed00020d272b08be23dd73014a3
Robert D Gauthier <rgauthie@gmail.com>
2018-03-16 16:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function currentLine (katzDeliLine){
   if (katzDeliLine.length === 0) {
     lineString += "empty."
   } else {
+    lineString += ":";
     for (let i = 0; i < katzDeliLine.length; i++) {
       lineString += `${i+1}. ${katzDeliLine[i]}`;
       if (i < katzDeliLine.length - 1) {
